f3dd0aec55f0b39d42d6bb0247706b0ae64df448
Olívia Gomes Martins <108308832+oliviagmartins@users.noreply.github.com>
2024-08-17 19:18:50 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,8 +47,6 @@ if vendas is not None:
 if vendas is not None:
     df_vendas['Qtd_Vendas'] = df_vendas.groupby('cli_codigo')['cli_codigo'].transform('count')
 
-st.write(df_vendas)
-
 # Data Preparation
 if 'df_vendas' in globals():
     vendas_cliente = df_vendas.groupby('cli_codigo')[['Vlr_Liquido', 'Qtd_Vendas', 'N_Produtos', 'Vlr_Desconto']].sum().reset_index()
