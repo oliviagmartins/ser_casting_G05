3e14073260b90a2ab29b83f35e266fa6db9bf30e
Olívia Gomes Martins <108308832+oliviagmartins@users.noreply.github.com>
2024-08-17 19:58:08 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -200,13 +200,13 @@ st.dataframe(feature_importance_df)
 st.bar_chart(feature_importance_df.set_index('Feature')['Importance'])
 
     # Plot Predicted vs Actual Values
-    #st.write("Predicted vs Actual Values:")
-    #plt.figure(figsize=(8, 6))
-    #plt.scatter(y_test, y_pred, alpha=0.5)
-    #plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], color='red', linestyle='--')  # Line of perfect fit
-    #plt.xlabel('Actual Values')
-    #plt.ylabel('Predicted Values')
-    #plt.title('Predicted vs Actual Values')
+st.write("Predicted vs Actual Values:")
+plt.figure(figsize=(8, 6))
+plt.scatter(y_test, y_pred, alpha=0.5)
+plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], color='red', linestyle='--')  # Line of perfect fit
+plt.xlabel('Actual Values')
+plt.ylabel('Predicted Values')
+plt.title('Predicted vs Actual Values')
     
     # Render plot in Streamlit
-    #st.pyplot(plt)
+st.pyplot(plt)
