2c2ae0cf4254a6bcf19d6eeb2a3612e7d95fd999
Olívia Gomes Martins <108308832+oliviagmartins@users.noreply.github.com>
2024-08-17 18:59:38 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,9 +44,9 @@ if vendas is not None:
     st.write("Preview do arquivo de vendas:")
     st.dataframe(df_vendas)
 
-# Proceed with any further data processing or model predictions
-
-# Data Preparation
+if vendas is not None:
+    df_vendas['Qtd_Vendas'] = df_vendas.groupby('cli_codigo')['cli_codigo'].transform('count').reset_index()
+    
 # Data Preparation
 if 'df_vendas' in globals():
     vendas_cliente = df_vendas.groupby('cli_codigo')[['Vlr_Liquido', 'Qtd_Vendas', 'N_Produtos', 'Vlr_Desconto']].sum().reset_index()
