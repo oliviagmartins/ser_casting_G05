1aecf639978dffa67a8246aa98038795a6494730
Olívia Gomes Martins <108308832+oliviagmartins@users.noreply.github.com>
2024-08-16 22:15:01 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import joblib
+import numpy as np
 
 # Load the model from the pickle file
 model = joblib.load('random_forest_model.pkl')
@@ -9,17 +10,15 @@ model = joblib.load('random_forest_model.pkl')
     return prediction[0]
 
 # Streamlit app UI
-st.title("Sales Prediction App")
+st.title("Previsão quantidade de vendas")
 
 # Collect user inputs
-input_data = []
-
-# Example input fields for the features your model uses
-feature_1 = st.number_input("Enter value for Feature 1")
-feature_2 = st.number_input("Enter value for Feature 2")
-# Add more input fields as needed for your features# Store the inputs in a list
-input_data.extend([feature_1, feature_2])
-
-# Make predictionsif st.button("Predict"):
+Vlr_Liquido = st.number_input("Insira o valor líquido")
+Quantidade_de_Acessos = st.number_input("Insira a quantidade de acessos")
+qtd_campanha = st.number_input("Insira a quantidade de campanha")
+qtd_treinamento = st.number_input("Insira a quantidade de treinamento")
+qtd_feedback = st.number_input("Insira a quantidade de feedback")
+# Add more input fields as needed# Make predictionif st.button("Predict"):
+    input_data = [Vlr_Liquido, Quantidade_de_Acessos, qtd_campanha, qtd_treinamento, qtd_feedback]  # Adjust this to match your model's input
     prediction = make_prediction(input_data)
-    st.write(f"The predicted sales amount is: {prediction}")
+    st.write(f"The predicted sales amount is: {prediction}"
