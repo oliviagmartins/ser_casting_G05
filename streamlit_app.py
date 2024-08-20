7cc0e5f9c34627e3a0db0b26ef7bdda73da0f96d
Olívia Gomes Martins <108308832+oliviagmartins@users.noreply.github.com>
2024-08-20 13:09:55 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,12 +29,15 @@ if response.status_code == 200:
 else:
     st.write("Falha em carregar o modelo.")
 
+
+#### ATÉ AQUI FUNCIONA ######
+
 # File uploaders for each CSV file
-#acessos = st.file_uploader("Upload do arquivo de acessos", type=["csv"], key="acessos")
-#campanha = st.file_uploader("Upload do arquivo de campanha", type=["csv"], key="campanha")
-#feedback = st.file_uploader("Upload do arquivo de feedback", type=["csv"], key="feedback")
-#treinamento = st.file_uploader("Upload do arquivo de treinamento", type=["csv"], key="treinamento")
-#vendas = st.file_uploader("Upload do arquivo de vendas", type=["csv"], key="vendas")
+acessos = st.file_uploader("Upload do arquivo de acessos", type=["csv"], key="acessos")
+campanha = st.file_uploader("Upload do arquivo de campanha", type=["csv"], key="campanha")
+feedback = st.file_uploader("Upload do arquivo de feedback", type=["csv"], key="feedback")
+treinamento = st.file_uploader("Upload do arquivo de treinamento", type=["csv"], key="treinamento")
+vendas = st.file_uploader("Upload do arquivo de vendas", type=["csv"], key="vendas")
 
 # Read the files into DataFrames
 #if acessos is not None:
