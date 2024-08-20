7142b500532a03391b7f849a94f8b864d2e73378
Olívia Gomes Martins <108308832+oliviagmartins@users.noreply.github.com>
2024-08-20 12:17:35 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ else:
     st.write("Falha em carregar o modelo.")
 
 
-#st.write("Tipo de modelo:", type(model))
+st.write("Tipo de modelo:", type(model))
 
 # File uploaders for each CSV file
 #acessos = st.file_uploader("Upload do arquivo de acessos", type=["csv"], key="acessos")
